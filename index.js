707dc87e33bad35af1260611af84079a09986b2c
Josh-Gotro <joshuagauthreaux@gmail.com>
2020-04-21 15:08:24 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("The Dom has loaded");
+
+    document.getElementById("text").textContent = "This is really cool!"
+});
+
+console.log(
+    "fire truck"
+);
