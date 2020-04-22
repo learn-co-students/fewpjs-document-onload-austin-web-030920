67bb2b29b970de8d5b46b2062c62a79a3a51f440
l.jackson96 <l.jackson96@outlook.com>
2020-04-21 17:08:46 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+let text = document.getElementById('text')
+//find the browser element 
+
+document.addEventListener("DOMContentLoaded", function() {
+    //create EventListener for the DOM loading
+    update();
+  });
+
+  //create function to change variable from line 2
+  // run function in EventListener
+  function update(){
+    text.textContent = "This is really cool!"
+  };
